30a4ae61e4a890696d070be602a4e3b9b3bafb4e
jannnice <e0969059@u.nus.edu>
2024-11-07 22:35:18 +0800

    language dropdown

--- frontend/src/components/collaboration/CodeEditor.jsx
+++ frontend/src/components/collaboration/CodeEditor.jsx
@@ -1,20 +1,43 @@
 import { useState } from "react"
 import { Editor } from "@monaco-editor/react"
 import "../../styles/code-editor.css"
+import { MenuItem, IconButton, Popper, Paper } from "@mui/material";
+import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
+import { ArrowDropDown } from "@mui/icons-material";
 
 const CodeEditor = ({ language, onMount }) => {
     const [value, setValue] = useState("");
+    const [selectedLanguage, setSelectedLanguage] = useState(language);
+
+    const popupState = usePopupState({ variant: 'popover', popupId: 'language-selector' });
+
+    const handleLanguageChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
 
     return (
         <div className="editor-container">
             <div className="editor-header">
-                <h2>{language}</h2>
+                <div className="language-selector">
+                    <IconButton {...bindTrigger(popupState)} sx={{ color: "#FFF", padding: "0"}}>
+                        {selectedLanguage} <ArrowDropDown />
+                    </IconButton>
+
+                    {/* Popper for dropdown menu */}
+                    <Popper {...bindMenu(popupState)} sx={{ zIndex: 2 }}>
+                        <Paper>
+                            <MenuItem onClick={() => { setSelectedLanguage('python'); popupState.close(); }}>Python</MenuItem>
+                            <MenuItem onClick={() => { setSelectedLanguage('javascript'); popupState.close(); }}>JavaScript</MenuItem>
+                            <MenuItem onClick={() => { setSelectedLanguage('java'); popupState.close(); }}>Java</MenuItem>
+                        </Paper>
+                    </Popper>
+                </div>
             </div>
             <div className="editor-content">
                 <Editor
                     theme="vs-light"
                     defaultLanguage="python"
-                    language={language}
+                    language={selectedLanguage}
                     onMount={onMount}
                     defaultValue=""
                     value={value}
